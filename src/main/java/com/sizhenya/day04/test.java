2ba54ad92c6d2addd3b2b4f9c1bcaf0838438507
whd <sizhenya@qq.com>
2020-03-25 13:36:49 +0800

    day01

---
+++ src/main/java/com/sizhenya/day04/test.java
@@ -0,0 +1,14 @@
+package com.sizhenya.day04;
+/*
+*
+*@author 司振亚
+*@date 2020年3月25日---下午1:32:19
+*
+*/
+public class test {
+
+	public static void main(String[] args) {
+		System.out.println("hellotest");
+	}
+}
+
